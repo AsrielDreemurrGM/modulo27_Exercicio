6edbe13a95793f3966901e819e3271b4c721f439
Eduardo Augusto <eduardo.motta.980315@gmail.com>
2024-11-15 20:16:32 -0300

    Criado Estrutura Base da Calculadora
    - Desenvolvido Funções Para Coleta Dos Valores Digitados;
    - Importado Bootstrap;

--- Calculadora/src/App.vue
+++ Calculadora/src/App.vue
@@ -1,11 +1,24 @@
 <script setup>
+import { reactive } from 'vue';
 
+import Calculadora from './components/Calculadora.vue';
+
+const estados = reactive({
+    valor1: 0,
+    valor2: 0,
+})
+
+function getPrimeiroNumero(valor) {
+    estados.valor1 = valor;
+}
+
+function getSegundoNumero(valor) {
+    estados.valor2 = valor;
+}
 </script>
 
 <template>
-
+    <div class="container" style="height: 100vh">
+        <Calculadora :valor-segundo-campo="getSegundoNumero" :valor-primeiro-campo="getPrimeiroNumero"/>
+    </div>
 </template>
-
-<style scoped>
-
-</style>
---
+++ Calculadora/src/components/Calculadora.vue
@@ -0,0 +1,21 @@
+<script setup>
+    const props = defineProps(['valorPrimeiroCampo', 'valorSegundoCampo'])
+</script>
+
+<template>
+    <div class="row d-flex justify-content-center align-items-center bg-secondary" style="height: 100%;">
+        <div class="col-md-3">
+            <input type="number" class="form-control" @keyup="props.valorPrimeiroCampo($event.target.value)" placeholder="Digite o primeiro valor" required>
+            <input type="number" class="form-control mb-3 mt-3" @keyup="props.valorSegundoCampo($event.target.value)" placeholder="Digite o segundo valor" required>
+            <input type="number" class="form-control" placeholder="O resultado aparecerá aqui" disabled>
+        </div>
+        <div class="col-md-3">
+            <select>
+                <option value="/">Dividir</option>
+                <option value="*">Multiplicar</option>
+                <option value="+">Somar</option>
+                <option value="-">Subtrair</option>
+            </select>
+        </div>
+    </div>
+</template>
