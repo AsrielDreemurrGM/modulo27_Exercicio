6b03fd5da2c6edab57f91a5a6444c8a095655137
Eduardo Augusto <eduardo.motta.980315@gmail.com>
2024-11-15 22:14:10 -0300

    Desenvolvido Código Para Fazer os Cálculos
    - Customizado Select;
    - Adicionado Título "Calculadora";
    - Alterado Título e Idioma da Página;
    - Criado Função Para Fazer os Cálculos;
    - Alterado Funções Que Recuperam os Valores Digitados Para Garantir Que Sejam Números;

--- Calculadora/src/App.vue
+++ Calculadora/src/App.vue
@@ -4,21 +4,40 @@ import { reactive } from 'vue';
 import Calculadora from './components/Calculadora.vue';
 
 const estados = reactive({
-    valor1: 0,
-    valor2: 0,
+    valor1: 1,
+    valor2: 1,
+    operacao: '/',
+    resultado: 0,
 })
 
 function getPrimeiroNumero(valor) {
-    estados.valor1 = valor;
+    estados.valor1 = parseFloat(valor);
 }
 
 function getSegundoNumero(valor) {
-    estados.valor2 = valor;
+    estados.valor2 = parseFloat(valor);
+}
+
+function fazerConta() {
+    const { operacao, valor1, valor2 } = estados;
+
+    switch (operacao) {
+        case '*':
+            return (estados.resultado = valor1 * valor2);
+        case '-':
+            return (estados.resultado = valor1 - valor2);
+        case '/':
+            return (estados.resultado = valor2 !== 0 ? valor1 / valor2 : 'Erro (divisão por zero)');
+        case '+':
+            return (estados.resultado = valor1 + valor2);
+    }
 }
 </script>
 
 <template>
     <div class="container" style="height: 100vh">
-        <Calculadora :valor-segundo-campo="getSegundoNumero" :valor-primeiro-campo="getPrimeiroNumero"/>
+        <Calculadora :trocar-operacao="evento => estados.operacao = evento.target.value"
+            :valor-segundo-campo="getSegundoNumero" :valor-primeiro-campo="getPrimeiroNumero" :fazer-conta="fazerConta"
+            :resultado="estados.resultado" />
     </div>
 </template>
--- Calculadora/src/components/Calculadora.vue
+++ Calculadora/src/components/Calculadora.vue
@@ -1,17 +1,23 @@
 <script setup>
-    const props = defineProps(['valorPrimeiroCampo', 'valorSegundoCampo'])
+const props = defineProps(['valorPrimeiroCampo', 'valorSegundoCampo', 'trocarOperacao', 'fazerConta', 'resultado'])
 </script>
 
 <template>
     <div class="row d-flex justify-content-center align-items-center bg-secondary" style="height: 100%;">
         <div class="col-md-3">
-            <input type="number" class="form-control" @keyup="props.valorPrimeiroCampo($event.target.value)" placeholder="Digite o primeiro valor" required>
-            <input type="number" class="form-control mb-3 mt-3" @keyup="props.valorSegundoCampo($event.target.value)" placeholder="Digite o segundo valor" required>
-            <input type="number" class="form-control" placeholder="O resultado aparecerá aqui" disabled>
+            <h1>Calculadora</h1>
+            <input type="number" class="form-control"
+                @keyup="props.valorPrimeiroCampo($event.target.value); props.fazerConta()"
+                placeholder="Digite o primeiro valor" required>
+            <input type="number" class="form-control mb-3 mt-3"
+                @keyup="props.valorSegundoCampo($event.target.value); props.fazerConta()"
+                placeholder="Digite o segundo valor" required>
+            <input :value="props.resultado" class="form-control" placeholder="O resultado aparecerá aqui"
+                disabled>
         </div>
         <div class="col-md-3 text-center">
             <p class="h6">Escolha o tipo de operacão:</p>
-            <select>
+            <select @change="props.trocarOperacao" class="rounded-1 border-black text-center">
                 <option value="/">Dividir</option>
                 <option value="*">Multiplicar</option>
                 <option value="+">Somar</option>
